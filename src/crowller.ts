7c4bb66b0afcf7abd9b519ac7db590ba53835ac1
jp93 <jiangpeng193@163.com>
2020-12-16 09:39:01 +0800

    初始化爬取到html

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,1 +1,15 @@
-console.log('hellow world')
+import superagent from 'superagent'
+class Crowller {
+  private secret = 'x3b174jsx'
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private rawHtml = ''
+  async getRawHtml() {
+    const result = await superagent.get(this.url)
+    console.log('result', result)
+  }
+  constructor() {
+    console.log('constructor')
+    this.getRawHtml()
+  }
+}
+const crowller = new Crowller()
